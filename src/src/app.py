d74cd3e74514cd7f9d075c0131316d94d3840c0f
erikssod <daniele@ansto.gov.au>
2021-02-09 15:59:47 +1100

    init done

--- src/src/app.py
+++ src/src/app.py
@@ -1,4 +1,20 @@
 import pandas as pd
 import numpy as np
-import itertools
+import itertools, yaml, sys, pathlib
+import logging, logging.config
 
+class triornot():
+    def __init__(self) -> None:
+        p1 = pathlib.Path('./src/src/logging.yaml')
+        with open(p1,'r') as f:
+            logconf = yaml.safe_load(f)
+        logging.config.dictConfig(logconf)
+        self.logger = logging.getLogger('simpleExample')
+
+        p2 = pathlib.Path('./src/src/config.yaml')
+        with open(p2,'r') as f:
+            self.cfg = yaml.load(f, Loader=yaml.FullLoader)
+
+        self.logger.info('Hello world!')
+
+tri = triornot()
