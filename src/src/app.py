4410c4d91533ebf813160aacb617b94c59a02407
erikssod <daniele@ansto.gov.au>
2021-02-12 16:57:41 +1100

    Comments and cleanup

--- src/src/app.py
+++ src/src/app.py
@@ -28,45 +28,62 @@ class triornot():
         self.df = self.df.drop(axis=0, index=['HKLF','END'])
         self.logger.debug(self.df)
 
-    def extractSG(self) -> None:
+    def extractCell(self) -> None:
+        '''
+        This is a mess. Would rather keep everything as dataframes
+        or series. Meh. Works for now. 
+        '''
         p = pathlib.Path(self.cfg['resfile'])
         with open(p,'r') as f:
             string = f.readlines()
 
-        sg = {}
-        _sg = string[3].split()
-        sg['a'] = float(_sg[2])
-        sg['b'] = float(_sg[3])
-        sg['c'] = float(_sg[4])
-        sg['alpha'] = float(_sg[5])
-        sg['beta'] = float(_sg[6])
-        sg['gamma'] = float(_sg[7])
-        self.sg = sg
-        self.logger.debug(sg)
+        cell = {}
+        _cell = string[3].split()
+        cell['a'] = float(_cell[2])
+        cell['b'] = float(_cell[3])
+        cell['c'] = float(_cell[4])
+        cell['alpha'] = float(_cell[5])
+        cell['beta'] = float(_cell[6])
+        cell['gamma'] = float(_cell[7])
+        self.cell = cell
+        self.logger.debug(cell)
 
     def makeCombinations(self) -> None:
+        '''
+        Make list of every possible pair of atoms
+        '''
         self.listToCheck = itertools.combinations(self.df.index,2)
 
-    def calc(self,sg:dict,pair:list) -> float:
-        delta = self.df.loc[pair[0]] - self.df.loc[pair[1]]
+    def calc(self,cell:dict,pair:list) -> float:
+        '''
+        Turn absolute difference of pair of coordinates (x,y,z), and turn it into
+        a distance between the two atoms. Works directly in unit cell space.
+        '''
+        delta = abs(self.df.loc[pair[0]] - self.df.loc[pair[1]])
 
-        d2 = (delta['x'] * sg['a'])**2 + \
-             (delta['y'] * sg['b'])**2 + \
-             (delta['z'] * sg['c'])**2 + \
-             (2 * sg['b']*sg['c']*delta['y']*delta['z']*np.cos(np.radians(sg['alpha']))) + \
-             (2 * sg['a']*sg['c']*delta['x']*delta['z']*np.cos(np.radians(sg['beta']))) +  \
-             (2 * sg['a']*sg['b']*delta['x']*delta['y']*np.cos(np.radians(sg['gamma'])))
+        d2 = (delta['x'] * cell['a'])**2 + \
+             (delta['y'] * cell['b'])**2 + \
+             (delta['z'] * cell['c'])**2 + \
+             (2 * cell['b']*cell['c']*delta['y']*delta['z']*np.cos(np.radians(cell['alpha']))) + \
+             (2 * cell['a']*cell['c']*delta['x']*delta['z']*np.cos(np.radians(cell['beta']))) +  \
+             (2 * cell['a']*cell['b']*delta['x']*delta['y']*np.cos(np.radians(cell['gamma'])))
 
         return d2**(1/2)
     
     def getDistBetweenNodes(self) -> None:
+        '''
+        Calls calc() on every pair of atoms, and creates a new dataframe. 
+        '''
         dist = []
         for i in self.listToCheck:
-            dist.append([i[0], i[1], self.calc(self.sg,i)])
+            dist.append([i[0], i[1], self.calc(self.cell,i)])
         self.dfdist = pd.DataFrame(dist,columns=['atom1','atom2','dist'])
         self.logger.debug(self.dfdist)
 
     def rejectOutliers(self) -> None:
+        '''
+        First round of rejections based purely on distance between atoms. 
+        '''
         triangle = self.cfg['triangle']
         min, max = triangle['side']-triangle['error'],\
                    triangle['side']+triangle['error']
@@ -77,16 +94,25 @@ class triornot():
         self.logger.debug(self.hits)
 
     def buildGraph(self) -> None:
+        '''
+        Takes remanining pairs of atoms and builds a graph from their relationships. 
+
+        In a graphical evironment, this one could be plotted to check for triangles
+        using matplotlib by calling somethinglike nx.plot(self.G)
+        '''
         for i,_ in enumerate(self.hits.index):
             a,b,_ = self.hits.iloc[i].values
             self.G.add_edges_from([(a,b)])
         self.logger.debug(f'added {list(self.G).__len__()} nodes to graph.')
 
     def pruneGraph(self) -> None:
-        """
+        '''
+        Looks for cyclical (i.e. closed, undirectional parts of the graph) elements,
+        and checks if they are triangles (i.e. hat drei Ecken). 
+
         Note that nx.find_cycle only returns the FIRST cycle it finds, not ALL.
         So, look, report, delete node, then check again for another cycle. 
-        """
+        '''
         while len(list(self.G)) > 0:
             try:
                 cycle = nx.find_cycle(self.G)
@@ -105,13 +131,16 @@ class triornot():
 
 
     def report(self):
+        '''
+        Could be used to clean up results. Not in use.
+        '''
         pass
 
     
 
 tri = triornot()
 tri.readResFile()
-tri.extractSG()
+tri.extractCell()
 tri.makeCombinations()
 tri.getDistBetweenNodes()
 tri.rejectOutliers()
